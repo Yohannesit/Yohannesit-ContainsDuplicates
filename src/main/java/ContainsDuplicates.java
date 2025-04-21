8970bc72a6555b4a3c5f7fabfadae827a91606d7
Yohannes Itanna <itannayohannes@gmail.com>
2025-04-21 18:09:23 +0000

    Completed Lab Contains Duplicates

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -8,6 +8,17 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        
+        java.util.Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length ; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
         return false;
     }
 }
